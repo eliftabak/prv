07f79a8351bacde391167d9575d255bd51685ee8
eliftabak <elifchorghay@gmail.com>
2020-09-01 10:46:59 +0300

    updating the style

--- wp-content/themes/prv/resources/views/partials/content-sorular-konusuyor.blade.php
+++ wp-content/themes/prv/resources/views/partials/content-sorular-konusuyor.blade.php
@@ -6,24 +6,23 @@ $home = App::home_url_with_slash();
   <div class="row">
     <div class="col-12">
       <div class="first-page">
-        <img src="{{$home}}wp-content/themes/prv/resources/assets/images/sorular-konusuyor-sidebar.png"
-          class="side-block" />
-        <div class="title-texts-container">
-          <div class="box">
-            <img src="{{$home}}wp-content/themes/prv/resources/assets/images/dots.png" alt="22x6dots"
-              class="dots-image">
-            <div class="text">
-              <h1 class="blue-text">Mavi</h1>
+        <div class="row title-texts-container">
+          <div class="col-8">
+            <div class="box">
+              <img src="{{$home}}wp-content/themes/prv/resources/assets/images/dots.png" alt="22x6dots"
+                class="dots-image">
+              <div class="text">
+                <h1 class="blue-text">Mavi</h1>
+              </div>
+            </div>
+            <div class="second-text-line">
+              <h1 class="text-inside">Sayfalar</h1>
             </div>
           </div>
-          <div class="second-text-line">
-            <h1 class="text-inside">Sayfalar</h1>
+          <div class="col-4">
+            <img src="{{$home}}wp-content/themes/prv/resources/assets/images/sorular-konusuyor.png"
+              alt="sorular konusuyor" class="sorular-image">
           </div>
-        </div>
-        <img src="{{$home}}wp-content/themes/prv/resources/assets/images/sorular-konusuyor.png" alt="sorular konusuyor"
-          class="sorular-image">
-        <div class="logo-questions-page">
-          <img src="{{$home}}wp-content/themes/prv/resources/assets/images/pruva-akademi-logo.png" alt="pruva-logo">
         </div>
       </div>
       <!----End of first page----->
